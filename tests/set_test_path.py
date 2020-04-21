9f97a6b03e86a3162b50462be9c838548508843e
GermainPereira <pereira.germain@outlook.com>
2020-04-21 09:45:56 -0300

    Improved sys.path append  message for tests

--- tests/set_test_path.py
+++ tests/set_test_path.py
@@ -3,4 +3,4 @@ import sys
 
 project_root_path_name = "/your_appointment"
 sys.path.insert(0, (os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) + project_root_path_name )
-print(f"\nPath to be tested setted to: {sys.path}\n\n")
+print(f"\nMain test path setted to: {sys.path[0]}\n\n")
