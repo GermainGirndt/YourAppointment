5d78252ec9725d223e539e580f6f44fcbadd1890
GermainPereira <pereira.germain@outlook.com>
2020-04-21 10:36:30 -0300

    Improved validators verification system

--- your_appointment/console/console_getter.py
+++ your_appointment/console/console_getter.py
@@ -5,27 +5,23 @@ class ConsoleGetter():
     @staticmethod
     def get_student_forename():
         student_forename = input("Enter the student's forename: ")
-        Validators().raise_type_error_if_not_string(student_forename)
         validated_student_forename = Validators().validate_student_forename(student_forename)
         return validated_student_forename
 
     @staticmethod
     def get_student_surname():
         student_surname = input("Enter the student's surname: ")
-        Validators().raise_type_error_if_not_string(student_surname)
         validated_student_surname = Validators().validate_student_surname(student_surname)
         return validated_student_surname
 
     @staticmethod
     def get_student_birthday():
         student_birthday = input("Enter the student's birthday (DD-MM-YYYY): ")
-        Validators().raise_type_error_if_not_string(student_birthday)
         return student_birthday
 
     @staticmethod
     def get_student_id_number():
         student_id_number = input("Enter the student's ID number: ")
-        Validators().raise_type_error_if_not_string(student_id_number)
         validated_student_id_number = Validators().validate_id_number(student_id_number)
         return validated_student_id_number
 
--- your_appointment/validators/validators.py
+++ your_appointment/validators/validators.py
@@ -36,17 +36,20 @@ class Validators():
 
     @staticmethod
     def validate_student_forename(student_forename_to_validate):
+        Validators().raise_type_error_if_not_string(student_forename_to_validate)
         checks = 0
         while checks < 2:
             while not Validators().is_alpha_or_has_spaces(student_forename_to_validate):
                 checks = 0
                 print("Invalid input. The forename muss contain only letters or spaces.")
                 student_forename_to_validate = input("Enter the student's forename: ")
+                Validators().raise_type_error_if_not_string(student_forename_to_validate)
             checks += 1
             while len(student_forename_to_validate) > 20:
                 checks = 0
                 print("Invalid input. The forename may not be longer than 25 characters.")
                 student_forename_to_validate = input("Enter the student's forename: ")
+                Validators().raise_type_error_if_not_string(student_forename_to_validate)
             checks += 1
         Validators().raise_type_error_if_not_string(student_forename_to_validate)
         return student_forename_to_validate
@@ -54,46 +57,51 @@ class Validators():
 
     @staticmethod
     def validate_student_surname(student_surname_to_validate):
+        Validators().raise_type_error_if_not_string(student_surname_to_validate)
         checks = 0
         while checks < 2:
             while not Validators().is_alpha_or_has_spaces(student_surname_to_validate):
                 checks = 0
                 print("Invalid input. The surname muss contain only letters or spaces.")
                 student_surname_to_validate = input("Enter the student's surname: ")
+                Validators().raise_type_error_if_not_string(student_surname_to_validate)
             checks += 1
             while len(student_surname_to_validate) > 20:
                 checks = 0
                 print("Invalid input. The surname may not be longer than 25 characters.")
                 student_surname_to_validate = input("Enter the student's surname: ")
+                Validators().raise_type_error_if_not_string(student_surname_to_validate)
             checks += 1
-        Validators().raise_type_error_if_not_string(student_surname_to_validate)
         return student_surname_to_validate
 
     @staticmethod
     def validate_birthday(date_to_validate):
+        Validators().raise_type_error_if_not_string(date_to_validate)
         while True:
             try:
                 datetime.datetime.strptime(date_to_validate, '%d-%m-%Y')
             except ValueError:
                 print("Invalid input. The birthdate muss comply to the required format")
                 date_to_validate = input("Enter the student's birthday (DD-MM-YYYY): ")
-            else:
                 Validators().raise_type_error_if_not_string(date_to_validate)
+            else:
                 return date_to_validate #noy using validated_date because it would be a datatime object
 
     @staticmethod
     def validate_id_number(id_number_to_validate):
+        Validators().raise_type_error_if_not_string(id_number_to_validate)
         checks = 2
         while checks < 2:
             while len(id_number_to_validate.replace("-","").replace(".","")) != 11:
                 checks = 0
                 print(f"Invalid input. The id number muss have 11 digits and should have comply to the required format")
                 id_number_to_validate = input("Enter the student's birthname (DD-MM-YYYY): ")
+                Validators().raise_type_error_if_not_string(id_number_to_validate)
             checks += 1
             while not id_number_to_validate.replace("-","").replace(".","").isnumeric():
                 checks = 0
                 print(f"Invalid input. The id number may only have numbers, points and dashes")
                 id_number_to_validate = input("Enter the student's birthname (DD-MM-YYYY): ")
+                Validators().raise_type_error_if_not_string(id_number_to_validate)
             checks += 1
-        Validators().raise_type_error_if_not_string(id_number_to_validate)
         return id_number_to_validate
