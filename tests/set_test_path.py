b5489154c2d98c532b2e29299a26841493f9a3c3
GermainPereira <pereira.germain@outlook.com>
2020-04-23 08:36:10 -0300

    Catched typo

--- tests/set_test_path.py
+++ tests/set_test_path.py
@@ -3,4 +3,4 @@ import sys
 
 project_root_path_name = "/your_appointment"
 sys.path.insert(0, (os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) + project_root_path_name )
-print(f"\nMain test path setted to: {sys.path[0]}\n\n")
+print(f"\nMain test path set to: {sys.path[0]}\n\n")
