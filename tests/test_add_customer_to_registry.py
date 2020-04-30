782f13c90abc378c8efeb7d70f29190535d87786
GermainPereira <pereira.germain@outlook.com>
2020-04-30 08:17:55 -0300

    Cleaned code

--- tests/test_add_customer_to_registry.py
+++ tests/test_add_customer_to_registry.py
@@ -11,19 +11,24 @@ class test_add_customer_to_memory(unittest.TestCase):
     customer_register_day = datetime.now().strftime("%Y-%m-%d")
     customer_register_time = datetime.now().strftime("%H:%M")
 
-    RIGHT_INPUTS_CUSTOMER_ID_0 = ["João", "da Silva", "23-05-1978", "297.586.890-10", "Rua Bom Sucesso, 487", "Casa",
-                                 "São Paulo", "SP"]
+    RIGHT_INPUTS_CUSTOMER_ID_0 = ["João", "da Silva", "23-05-1978",
+                                  "297.586.890-10", "Rua Bom Sucesso, 487",
+                                  "Casa", "São Paulo", "SP"]
 
-    RIGHT_RETURN_CUSTOMER_ID_0 = ["João", "da Silva", "23-05-1978", "297.586.890-10", "Rua Bom Sucesso, 487", "Casa",
-                                 "São Paulo", "SP", customer_register_day, customer_register_time, "Active"]
+    RIGHT_RETURN_CUSTOMER_ID_0 = ["João", "da Silva", "23-05-1978",
+                                  "297.586.890-10", "Rua Bom Sucesso, 487",
+                                  "Casa", "São Paulo", "SP",
+                                  customer_register_day, customer_register_time, "Active"]
 
-
-    RIGHT_INPUTS_CUSTOMER_ID_1 = ["Joana", "Silveira", "17-02-1972", "434.763.780-20", "Felicidade, 14", "Ap. 201",
+    RIGHT_INPUTS_CUSTOMER_ID_1 = ["Joana", "Silveira", "17-02-1972",
+                                  "434.763.780-20", "Felicidade, 14", "Ap. 201",
                                  "Rio de Janeiro", "RJ"]
 
 
-    RIGHT_RETURN_CUSTOMER_ID_1 = ["Joana", "Silveira", "17-02-1972", "434.763.780-20", "Felicidade, 14", "Ap. 201",
-                                 "Rio de Janeiro", "RJ", customer_register_day, customer_register_time, "Active"]
+    RIGHT_RETURN_CUSTOMER_ID_1 = ["Joana", "Silveira", "17-02-1972",
+                                  "434.763.780-20", "Felicidade, 14", "Ap. 201",
+                                 "Rio de Janeiro", "RJ",
+                                  customer_register_day, customer_register_time, "Active"]
 
     def setUp(self):
         self.ams_instance = Ams()
@@ -42,7 +47,8 @@ class test_add_customer_to_memory(unittest.TestCase):
         with mock.patch('builtins.input', side_effect=self.RIGHT_INPUTS_CUSTOMER_ID_1):
             self.ams_instance.add_customer_to_registry()
         self.assertEqual(self.RIGHT_RETURN_CUSTOMER_ID_1, self.ams_instance.customers_registry[1])
-        self.assertEqual([self.RIGHT_RETURN_CUSTOMER_ID_0, self.RIGHT_RETURN_CUSTOMER_ID_1], self.ams_instance.customers_registry)
+        self.assertEqual([self.RIGHT_RETURN_CUSTOMER_ID_0, self.RIGHT_RETURN_CUSTOMER_ID_1],
+                         self.ams_instance.customers_registry)
 
     def test_add_1_customer_fails_forename_value_error(self):
         wrong_forename_input = "João2"
@@ -84,7 +90,8 @@ class test_add_customer_to_memory(unittest.TestCase):
         wrong_customer_id_input = "24011978"
         inputed_inputs = self.RIGHT_INPUTS_CUSTOMER_ID_0[:]
         inputed_inputs.insert(3, wrong_customer_id_input)
-        expected_exception = "Invalid input. The customer id muss have 11 digits and comply to the required format\n"
+        expected_exception = "Invalid input. The customer id muss have 11 digits and " \
+                             "comply to the required format\n"
         with redirect_stdout(StringIO()) as stdout:
             with mock.patch('builtins.input', side_effect=inputed_inputs):
                 self.ams_instance.add_customer_to_registry()
--- tests/test_remove_customer_by_index.py
+++ tests/test_remove_customer_by_index.py
@@ -12,45 +12,53 @@ class test_remove_customer_by_index(unittest.TestCase):
 		customer_register_day = datetime.now().strftime("%Y-%m-%d")
 		customer_register_time = datetime.now().strftime("%H:%M")
 
-		self.ams_instance = Ams()
-		self.right_inputs_customer_id_0 = ["João", "da Silva", "23-05-1978", "297.586.890-10", "Rua Bom Sucesso, 487", "Casa", "São Paulo", "SP", customer_register_day, customer_register_time, "Active"]
-		self.right_inputs_customer_id_1 = ["Joana", "Silveira", "17-02-1972", "434.763.780-20", "Felicidade, 14", "Ap. 201", "Rio de Janeiro", "RJ", customer_register_day, customer_register_time, "Active"]
+		self.RIGHT_INPUTS_CUSTOMER_ID_0 = ["João", "da Silva", "23-05-1978",
+										   "297.586.890-10", "Rua Bom Sucesso, 487",
+										   "Casa", "São Paulo", "SP",
+										   customer_register_day, customer_register_time, "Active"]
 
-		with mock.patch('builtins.input', side_effect=self.right_inputs_customer_id_0):
+		self.RIGHT_INPUTS_CUSTOMER_ID_1 = ["Joana", "Silveira", "17-02-1972",
+										   "434.763.780-20", "Felicidade, 14",
+										   "Ap. 201", "Rio de Janeiro", "RJ",
+										   customer_register_day, customer_register_time, "Active"]
+
+		self.ams_instance = Ams()
+		with mock.patch('builtins.input', side_effect=self.RIGHT_INPUTS_CUSTOMER_ID_0):
 			self.ams_instance.add_customer_to_registry()
-		with mock.patch('builtins.input', side_effect=self.right_inputs_customer_id_1):
+		with mock.patch('builtins.input', side_effect=self.RIGHT_INPUTS_CUSTOMER_ID_1):
 			self.ams_instance.add_customer_to_registry()
 		self.assertEqual(len(self.ams_instance.customers_registry), 2) #Checks
-		self.assertIn(self.right_inputs_customer_id_0, self.ams_instance.customers_registry)
-		self.assertIn(self.right_inputs_customer_id_1, self.ams_instance.customers_registry)
+		self.assertIn(self.RIGHT_INPUTS_CUSTOMER_ID_0, self.ams_instance.customers_registry)
+		self.assertIn(self.RIGHT_INPUTS_CUSTOMER_ID_1, self.ams_instance.customers_registry)
 
 	def test_remove_1_customer_by_index_passes(self):
 		rightId = "0"
 		with mock.patch('builtins.input', side_effect=rightId):
 			self.ams_instance.remove_customer_by_index()
-		self.assertEqual(len(self.ams_instance.customers_registry),1)
-		self.assertNotIn(self.right_inputs_customer_id_0, self.ams_instance.customers_registry)
+		self.assertEqual(1, len(self.ams_instance.customers_registry))
+		self.assertNotIn(self.RIGHT_INPUTS_CUSTOMER_ID_0, self.ams_instance.customers_registry)
 
 	def test_remove_1_customer_by_index_passes2(self):
 		rightId = ["1"]
 		with mock.patch('builtins.input', side_effect=rightId):
 			self.ams_instance.remove_customer_by_index()
-		self.assertEqual(len(self.ams_instance.customers_registry), 1)
-		self.assertNotIn(self.right_inputs_customer_id_1, self.ams_instance.customers_registry)
+		self.assertEqual(1, len(self.ams_instance.customers_registry))
+		self.assertNotIn(self.RIGHT_INPUTS_CUSTOMER_ID_1, self.ams_instance.customers_registry)
 
 
 	def test_remove_1_customer_by_index_fails_inexisting_index(self):
 		wrong_input_id = "200"
 		right_input_id = "0"
 		inputed_inputs = [wrong_input_id, right_input_id]
-		expected_exception = f"The inputed value is out of range. Please input a index from 0 to {len(self.ams_instance.customers_registry)}"
+		expected_exception = f"The inputed value is out of range." \
+							 f"Please input a index from 0 to {len(self.ams_instance.customers_registry)}"
 		with redirect_stdout(StringIO()) as stdout:
 			with mock.patch('builtins.input', side_effect=inputed_inputs):
 				self.ams_instance.remove_customer_by_index()
 		printed_messages = stdout.getvalue()
 		self.assertIn(expected_exception, printed_messages)
 		self.assertEqual(len(self.ams_instance.customers_registry), 1)
-		self.assertNotIn(self.right_inputs_customer_id_0, self.ams_instance.customers_registry)
+		self.assertNotIn(self.RIGHT_INPUTS_CUSTOMER_ID_0, self.ams_instance.customers_registry)
 
 
 	def test_remove_1_customer_by_index_fails_type_error(self):
@@ -59,8 +67,8 @@ class test_remove_customer_by_index(unittest.TestCase):
 			with self.assertRaises(TypeError):
 				self.ams_instance.remove_customer_by_index()
 		self.assertEqual(len(self.ams_instance.customers_registry), 2)
-		self.assertIn(self.right_inputs_customer_id_0, self.ams_instance.customers_registry)
-		self.assertIn(self.right_inputs_customer_id_1, self.ams_instance.customers_registry)
+		self.assertIn(self.RIGHT_INPUTS_CUSTOMER_ID_0, self.ams_instance.customers_registry)
+		self.assertIn(self.RIGHT_INPUTS_CUSTOMER_ID_1, self.ams_instance.customers_registry)
 
 	def test_remove_1_customer_by_index_fails_value_error(self):
 		wrong_input = ["lala"]
@@ -68,15 +76,15 @@ class test_remove_customer_by_index(unittest.TestCase):
 			with self.assertRaises(ValueError):
 				self.ams_instance.remove_customer_by_index()
 		self.assertEqual(len(self.ams_instance.customers_registry), 2)
-		self.assertIn(self.right_inputs_customer_id_0, self.ams_instance.customers_registry)
-		self.assertIn(self.right_inputs_customer_id_1, self.ams_instance.customers_registry)
+		self.assertIn(self.RIGHT_INPUTS_CUSTOMER_ID_0, self.ams_instance.customers_registry)
+		self.assertIn(self.RIGHT_INPUTS_CUSTOMER_ID_1, self.ams_instance.customers_registry)
 
 	def test_remove_2_customers_by_index_passes(self):
 		rightId = ["0","1"]
 		with mock.patch('builtins.input', side_effect=rightId):
 			self.ams_instance.remove_customer_by_index()
-		self.assertEqual(len(self.ams_instance.customers_registry),1)
-		self.assertNotIn(self.right_inputs_customer_id_0, self.ams_instance.customers_registry)
+		self.assertEqual(1, len(self.ams_instance.customers_registry))
+		self.assertNotIn(self.RIGHT_INPUTS_CUSTOMER_ID_0, self.ams_instance.customers_registry)
 
 
 	def tearDown(self):
